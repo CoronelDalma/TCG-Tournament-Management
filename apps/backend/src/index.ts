8e38e766ccfe746af0c854383055dae24d2a8818
CoronelDalma <coronel.dg@gmail.com>
2025-10-19 00:43:44 -0300

    configured to test endpoints

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,8 +2,3 @@ import { saludo  } from "../../../domain/dist/index.js";
 import { registerUser, loginUser, UserService, User, UserRole } from "../../../domain/dist/index.js";
 console.log("Backend service started");
 console.log(saludo);
-import app from "./server.js"
-
-app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
-});
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -14,4 +14,8 @@ app.use(express.json());
 app.use('/api/users', userRoutes);
 app.use("/api/auth", authRoutes);
 
+app.listen(3000, () => {
+    console.log('Server running on http://localhost:3000');
+});
+
 export default app;
