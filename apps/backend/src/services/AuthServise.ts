d94fef473ab1b578abcc394f0167afe26aab257a
CoronelDalma <coronel.dg@gmail.com>
2025-10-23 12:15:14 -0300

    fix:  AuthServiceImplementation, JWT_SECRET

--- apps/backend/src/services/AuthServise.ts
+++ apps/backend/src/services/AuthServise.ts
@@ -3,7 +3,14 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 export class AuthServiceImplementation implements AuthService {
-    secret = process.env.JWT_SECRET;
+    private secret: string;
+    
+    constructor() {
+        if (!process.env.JWT_SECRET) {
+            throw new Error("JWT_SECRET no está definido")
+        }
+        this.secret = process.env.JWT_SECRET;
+    }
 
     async hashPassword(password: string): Promise<string> {
         return bcrypt.hash(password, 10);
@@ -13,6 +20,6 @@ export class AuthServiceImplementation implements AuthService {
     }
     async generateToken(userId: string, role: string): Promise<string> {
         if (!this.secret) throw new Error("JWT_SECRET no está definido");
-        return jwt.sign({id: userId, role}, process.env.JWT_SECRET!, { expiresIn: "1h"})
+        return jwt.sign({id: userId, role}, this.secret, { expiresIn: "1h"})
     }
 }
